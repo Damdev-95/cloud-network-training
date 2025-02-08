b3a6e5c4365b67dcaa3f201bdfea5f8c3bda02b5
Sulaiman Olubiyi <71001536+Damdev-95@users.noreply.github.com>
2025-02-08 14:11:09 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,126 @@
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "${var.environment}-VPC"
+    Environment = var.environment
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.environment}-IGW"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-public-subnet"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "${var.aws_region}b"
+
+  tags = {
+    Name        = "${var.environment}-private-subnet"
+    Environment = var.environment
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "${var.environment}-public-rt"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.environment}-private-rt"
+    Environment = var.environment
+  }
+}
+
+# Route Table Associations
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+# Security Group
+resource "aws_security_group" "web-sg" {
+  name        = "${var.environment}-web-sg"
+  description = "Security group for EC2 instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.environment}-ec2-sg"
+    Environment = var.environment
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web_server" {
+  ami           = "ami-0e1bed4f06a3b463d" 
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public.id
+  
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name              = var.key_name
+
+  tags = {
+    Name        = "${var.environment}-web-server"
+    Environment = var.environment
+  }
+}
+
+
+
